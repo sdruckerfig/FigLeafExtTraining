211ff23364442e03ea19305f2ec1624a1af848d1
Steve Drucker <sdrucker@figleaf.com>
2015-02-17 21:49:43 -0600

    Unit 8 student files

---
+++ extjs5/walk/walk8-3/app/view/patientinfo/graphs/Pie.js
@@ -0,0 +1,37 @@
+Ext.define("PatientChart.view.patientinfo.graphs.Pie", {
+	extend: "Ext.chart.PolarChart",
+	alias: 'widget.dietpiechart',
+	requires: [
+		'PatientChart.view.patientinfo.graphs.PieController',
+		'Ext.chart.interactions.Rotate',
+		'Ext.chart.interactions.ItemHighlight',
+		'Ext.chart.series.Pie'
+	],
+	controller: "patientinfo-graphs-pie",
+	bind: {
+		store: '{PatientDiet}'
+	},
+	interactions: ['rotate'],
+	legend: true,
+	series: {
+		type: 'pie',
+		label: {
+			field: 'label',
+		},
+		xField: 'qty',
+		donut: 30,
+		tooltip: {
+			trackMouse: true,
+			renderer: function(record, data) {
+				// calculate and display percentage on hover
+				var total = 0;
+				record.store.each(function(rec) {
+					total += rec.get('qty');
+				});
+				this.setHtml(
+					record.get('label') + ": " + Math.round(record.get('qty') / total * 100) + '%'
+				);
+			}
+		}
+	}
+});
