c237a4e55719387a741abc235bb2004d6f813d8e
Steve Drucker <sdrucker@figleaf.com>
2015-03-02 15:42:32 -0600

    Walkthrough 5-3 fix

--- extjs5/walk/walk5-3/app/view/research/clinicaltrials/ClinicalTrialsModel.js
+++ extjs5/walk/walk5-3/app/view/research/clinicaltrials/ClinicalTrialsModel.js
@@ -1,9 +1,10 @@
 Ext.define('PatientChart.view.research.clinicaltrials.ClinicalTrialsModel', {
 	extend: 'Ext.app.ViewModel',
 	alias: 'viewmodel.research-clinicaltrials-clinicaltrials',
-
+	requires: ['PatientChart.model.ClinicalTrial'],
 	stores: {
 		ClinicalTrials: {
+			autoLoad: true,
 			model: 'PatientChart.model.ClinicalTrial',
 			remoteFilter: true,
 			remoteSort: true,
